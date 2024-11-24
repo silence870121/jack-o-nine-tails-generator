d3cc53862d14ab393ca7c92a20e5b9e7a9ad2988
Herbert (WEI-CHE, Chung) <82174729+silence870121@users.noreply.github.com>
2024-11-24 16:44:25 +0800

    Add files via upload

---
+++ js/About-D9cDNk1g.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-bSjb2ad4.js";const t={};function n(r,s){return o(),c("h2",null," About ")}const a=e(t,[["render",n]]);export{a as default};
---
+++ js/Contact-DbXVntid.js
@@ -0,0 +1,1 @@
+import{_ as c,c as t,o as e}from"./index-bSjb2ad4.js";const n={};function o(r,a){return e(),t("h2",null," Contact ")}const _=c(n,[["render",o]]);export{_ as default};
